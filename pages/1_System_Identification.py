ef38e46adc465d71b592b5c4f01ba04196f89cef
jceneziojr <julio.cesar.enezio@gmail.com>
2022-09-08 17:37:51 -0300

    conteúdo adicionado

--- pages/1_System_Identification.py
+++ pages/1_System_Identification.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-'hi'
+col1, esp, col2, esp, col3 = st.columns([2,1,2,1,2])
+
+with col1:
+    st.number_input("X Lag", 0, 10, key="xlag")
+
+with col2:
+    st.number_input("Y Lag", 0, 10, key="ylag")
+
+with col3:
+    st.number_input("NL Degree", 1, 5, key="degree")
+
+if st.checkbox("e"):
+    st.number_input("e Lag", 0, 10, key="elag")
+else: st.session_state.elag = 0
+
+st.selectbox("Estimação de parâmetros e regressores", ["m1", "m2", "m3", "m4"], key="method")
+
+st.session_state
--- sysidentpy_gui.py
+++ sysidentpy_gui.py
@@ -8,6 +8,3 @@ with col1:
 
 with col2:
     st.file_uploader("Dados Validação", key='val_data')
-
-
-
